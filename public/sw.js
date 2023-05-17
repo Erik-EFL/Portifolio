4735bcc67e1d61f04b6a15c34bfc4bb92980fed1
erik lima <erik.lima.f@outlook.com>
2023-05-17 16:12:13 -0300

    feat: :sparkles: Impruve service worker
    adding new validations on function

--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,64 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  // Cache-first strategy
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const cacheCopy = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, cacheCopy);
+          });
+        }
+        return networkResponse;
+      });
+    })
+  );
+
+  // Network-first strategy
+  /*
+  event.respondWith(
+    fetch(request).then((networkResponse) => {
+      if (networkResponse && networkResponse.status === 200) {
+        const cacheCopy = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(request, cacheCopy);
+        });
+      }
+      return networkResponse;
+    }).catch(() => {
+      return caches.match(request);
+    })
+  );
+  */
+
+  // Stale-while-revalidate strategy
+  /*
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(request).then((cachedResponse) => {
+        const fetchPromise = fetch(request).then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200) {
+            cache.put(request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchPromise;
+      });
+    })
+  );
+  */
+});
+
+/* self.addEventListener('fetch', event => {
   if (!(event.request.url.startsWith(self.location.origin)) || event.request.method !== 'GET') { return; }
   event.respondWith(async function() {
     try {
@@ -33,15 +90,16 @@ self.addEventListener('fetch', event => {
       console.log('Error:', error);
     }
   }());
-});
+}); */
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key.startsWith('my-web-app-cache-') && key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
+      return Promise.all(keys.map(key => {
+        if (key.startsWith('my-web-app-cache-') && key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
   event.waitUntil(self.clients.claim());
